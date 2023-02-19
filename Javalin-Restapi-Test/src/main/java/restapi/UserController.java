01213ab0107dc66b3599afc216bae4fd772eca68
Julie Wu <lianzhujun@lianzhujundeMacBook-Pro.local>
2023-02-19 19:01:51 +0800

    add Update and Create method

--- Javalin-Restapi-Test/src/main/java/restapi/UserController.java
+++ Javalin-Restapi-Test/src/main/java/restapi/UserController.java
@@ -1,6 +1,7 @@
 package restapi;
 
 import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
 
 import java.util.Set;
 
@@ -13,13 +14,13 @@ public class UserController {
     }
 
     public void listAll(Context context) {
-        Set<User> users = userService.users;
+        Set<User> users = userService.getAllUsers();
         context.json(users);
     }
 
     public void listOne(Context context) {
         String userIdFromBrowser = context.pathParam("userId");
-        User user = userService.getUser(userIdFromBrowser);
+        User user = userService.getUserById(userIdFromBrowser);
         if (user != null) {
             context.json(user);
         } else {
@@ -29,11 +30,26 @@ public class UserController {
     }
 
     public void create(Context context) {
-
+        User user = context.bodyValidator(User.class).get(); // will throw if the request body is not valid
+        if (userService.addUser(user)) {
+            context.status(201);
+        } else {
+            context.status(400);
+        }
     }
 
     public void update(Context context) {
-
+        String userIdFromBrowser = context.pathParam("userId");
+        if (userService.getUserById(userIdFromBrowser) == null) {
+            context.status(404);
+        } else {
+            User user = context.bodyValidator(User.class).get(); // will throw if the request body is not valid
+            if (userService.updateUser(userIdFromBrowser, user.name(), user.email())) {
+                context.status(204);
+            } else {
+                context.status(400);
+            }
+        }
     }
 
     public void delete(Context context) {
@@ -44,7 +60,6 @@ public class UserController {
         } else {
             context.status(404).result("user not found");
         }
-
     }
 
 }
--- Javalin-Restapi-Test/src/main/java/restapi/UserService.java
+++ Javalin-Restapi-Test/src/main/java/restapi/UserService.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class UserService {
 
-    public Set<User> users = new HashSet<>(
+    private final Set<User> users = new HashSet<>(
             List.of(
                     new User("1", "Sandy", "0987654321@email.com"),
                     new User("2", "Tom", "1122334455@email.com"),
@@ -14,17 +14,31 @@ public class UserService {
             )
     );
 
-    public Boolean removeUser(String userId) {
-        return users.removeIf(user -> user.id().equals(userId));
+    public Set<User> getAllUsers() {
+        return users;
     }
 
-    public User getUser(String userId) {
+    public User getUserById(String userId) {
         for (User user : users) {
             if (userId.equals(user.id())) {
                 return user;
             }
         }
         return null;
+    }
+
+    public boolean addUser(User user) {
+        return users.add(user);
+    }
+
+    public boolean updateUser(String id, String name, String email) {
+        if (name == null || email == null) return false;
+        removeUser(id);
+        return addUser(new User(id, name, email));
+    }
+
+    public boolean removeUser(String userId) {
+        return users.removeIf(user -> user.id().equals(userId));
     }
 
 }
